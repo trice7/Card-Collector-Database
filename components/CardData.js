07438c6af55c4d082f2df2d27bbeec13639af2fb
Thomas Rice <ricethomasj@gmail.com>
2023-09-02 12:57:29 -0500

    Pricing on page display. Added disclaimer pages

---
+++ components/CardData.js
@@ -0,0 +1,76 @@
+import Table from 'react-bootstrap/Table';
+import PropTypes from 'prop-types';
+
+const CardData = ({ obj }) => {
+  console.warn('on card page');
+  const priceKeys = Object.keys(obj?.tcgplayer.prices);
+  return (
+    <>
+      <Table bordered hover size="sm">
+        <tbody>
+          <tr>
+            <th>Name:</th>
+            <td>{obj.name}</td>
+          </tr>
+          <tr>
+            <th>Artist:</th>
+            <td>{obj.artist}</td>
+          </tr>
+          <tr>
+            <th>Set:</th>
+            <td>{obj.set?.name}</td>
+          </tr>
+          <tr>
+            <th>Rarity:</th>
+            <td>{obj.rarity}</td>
+          </tr>
+          <tr>
+            <th>Released:</th>
+            <td>{obj.set?.releaseDate}</td>
+          </tr>
+        </tbody>
+      </Table>
+
+      <h3>Prices</h3>
+
+      {priceKeys?.map((category) => (
+        <Table bordered hover size="sm">
+          <tbody>
+            <tr>
+              <th colSpan={2}>{category}</th>
+            </tr>
+            <tr>
+              <th>Low:</th>
+              <td>${obj.tcgplayer.prices[category].low}</td>
+            </tr>
+            <tr>
+              <th>Mid:</th>
+              <td>${obj.tcgplayer.prices[category].mid}</td>
+            </tr>
+            <tr>
+              <th>High:</th>
+              <td>${obj.tcgplayer.prices[category].high}</td>
+            </tr>
+            <tr>
+              <th>Market:</th>
+              <td>${obj.tcgplayer.prices[category].market}</td>
+            </tr>
+          </tbody>
+        </Table>
+      ))}
+      <p>Price as of: {obj.tcgplayer.updatedAt}</p>
+    </>
+  );
+};
+
+CardData.propTypes = {
+  obj: PropTypes.shape({
+    name: PropTypes.string,
+    set: PropTypes.string,
+    rarity: PropTypes.string,
+    artist: PropTypes.string,
+    tcgplayer: PropTypes.string,
+  }).isRequired,
+};
+
+export default CardData;
